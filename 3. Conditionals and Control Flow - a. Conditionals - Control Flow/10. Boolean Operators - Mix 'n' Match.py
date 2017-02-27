ff622ffb2f2f20d0152bf6e4886fa40cba2666ef
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-02-27 22:56:45 +0700

    Create 10. Boolean Operators - Mix 'n' Match.py

---
+++ 3. Conditionals and Control Flow - a. Conditionals - Control Flow/10. Boolean Operators - Mix 'n' Match.py
@@ -0,0 +1,16 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = not False and not True or True
+
+# Make me false!
+bool_three = 2 + 2 != 4 and 2 + 2 == 4
+
+# Make me true!
+bool_four = not(2*3 == 8) or(10**6 == 1000000) 
+
+# Make me true!
+bool_five = True and True and not False or False
