5bd19817c9304c2c20605bac3ddbd7bedb16730d
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-02-27 23:09:43 +0700

    Create 15. Review - The Big If.py

---
+++ 3. Conditionals and Control Flow - a. Conditionals - Control Flow/15. Review - The Big If.py
@@ -0,0 +1,14 @@
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if 1 == 0 or 0 == 0:    # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+        return True
+    elif 1!=1 and 1<=0:
+        # Keep going here.
+        # You'll want to add the else statement, too!
+        return False
+    else:
+        return True
+        
+print the_flying_circus()
