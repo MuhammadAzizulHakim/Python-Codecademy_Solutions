e23dc164fee47c0c1449e367de8dd070df697271
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-02 22:03:24 +0700

    Create 6. Planes, Hotels and Automobiles - Hey, You Never Know!.py

---
+++ 4. Functions - b. Taking a Vacation/6. Planes, Hotels and Automobiles - Hey, You Never Know!.py
@@ -0,0 +1,22 @@
+def hotel_cost(days):
+    return 140 * days
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+def rental_car_cost(days):
+    car_cost =  40 * days
+    if days >= 7:
+        return car_cost - 50
+    elif days >= 3:
+        return car_cost - 20
+    else:    
+        return car_cost
+def trip_cost(city,days,spending_money):
+    total = hotel_cost(days) + plane_ride_cost(city) + rental_car_cost(days) + spending_money
+    return total
