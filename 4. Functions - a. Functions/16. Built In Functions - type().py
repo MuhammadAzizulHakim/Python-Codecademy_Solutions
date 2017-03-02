87dd03991dc56539b038bd122fd5ae165eb5c9eb
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-02 17:11:38 +0700

    Create 16. Built In Functions - type().py

---
+++ 4. Functions - a. Functions/16. Built In Functions - type().py
@@ -0,0 +1,9 @@
+# Print out the types of an integer, a float,
+# and a string on separate lines below.
+
+print type(42)
+print type(99)
+print type(4.2)
+print type(1000.123)
+print type('spam')
+print type('Tong kosong nyaring bunyinya.')
