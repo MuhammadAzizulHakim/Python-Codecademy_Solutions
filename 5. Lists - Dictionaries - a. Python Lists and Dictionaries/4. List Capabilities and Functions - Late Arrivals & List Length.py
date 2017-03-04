8af7bdf03992a06be1a200f2b37f3ab86f196f36
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-04 13:36:27 +0700

    Create 4. List Capabilities and Functions - Late Arrivals & List Length.py

---
+++ 5. Lists - Dictionaries - a. Python Lists and Dictionaries/4. List Capabilities and Functions - Late Arrivals & List Length.py
@@ -0,0 +1,12 @@
+suitcase = [] 
+suitcase.append("sunglasses")
+
+# Your code here!
+suitcase.append("bathing suit")
+suitcase.append("underwear")
+suitcase.append("lotion")
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
