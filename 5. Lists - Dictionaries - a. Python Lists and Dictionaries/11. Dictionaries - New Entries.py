eae3e204573d87175371600962e978e5b47d6e86
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-04 18:48:18 +0700

    Create 11. Dictionaries - New Entries.py

---
+++ 5. Lists - Dictionaries - a. Python Lists and Dictionaries/11. Dictionaries - New Entries.py
@@ -0,0 +1,11 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Cireng'] = 1.50
+menu['Bala-bala'] = 1.50
+menu['Gehu'] = 1.50
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
