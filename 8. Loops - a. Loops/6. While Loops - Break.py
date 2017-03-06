ebcb3173c053ef9dda655ad1fbe2845279fb66f5
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-06 18:03:08 +0700

    Create 6. While Loops - Break.py

---
+++ 8. Loops - a. Loops/6. While Loops - Break.py
@@ -0,0 +1,15 @@
+count = 0
+
+while True:
+    print count
+    count += 1
+    if count >= 10:
+        break
+    
+count2 = 10
+
+while True:
+    print count2
+    count2 -= 1
+    if count2 <= 0:
+        break
