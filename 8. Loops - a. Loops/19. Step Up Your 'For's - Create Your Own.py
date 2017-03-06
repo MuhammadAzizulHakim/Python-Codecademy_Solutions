af23593cfe5962125064f76deef823a5f68092f2
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-06 21:18:28 +0700

    Update 19. Step Up Your 'For's - Create Your Own.py

--- 8. Loops - a. Loops/19. Step Up Your 'For's - Create Your Own.py
+++ 8. Loops - a. Loops/19. Step Up Your 'For's - Create Your Own.py
@@ -7,3 +7,13 @@ for a in animals:
     print 'A', a
 else:
     print 'A fine selections of animals!'
+
+#example 2:
+
+list_a = ['a', 'a', 'a', 'a', 'a', 'a', 'b', 'a']
+
+for index,letters in enumerate(list_a):
+    if letters == 'a':
+        print "I am No.", index + 1, letters
+else:
+    print "Oops, I am spy b."
