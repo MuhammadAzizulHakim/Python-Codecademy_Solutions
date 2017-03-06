5572102987451c4393709a4e3a2df5fd871f7ab1
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-06 09:48:45 +0700

    Create 6. Just Average - Just Weight and See.py

---
+++ 6. Student Becomes the Teacher - Student Becomes the Teacher/6. Just Average - Just Weight and See.py
@@ -0,0 +1,33 @@
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+    total = sum(numbers)
+    result = float(total)/len(numbers)
+    return result
+    print result
+    
+def get_average(student):
+    homework = 0.1 * average(student["homework"])
+    quizzes = 0.3 * average(student["quizzes"])
+    tests = 0.6 * average(student["tests"])
+    total = homework + quizzes + tests
+    return total
+    print total
