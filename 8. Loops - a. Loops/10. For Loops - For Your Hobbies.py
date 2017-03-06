af019f73a4e8379431329ce8cec65157d3af2cdf
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-06 18:13:14 +0700

    Create 10. For Loops - For Your Hobbies.py

---
+++ 8. Loops - a. Loops/10. For Loops - For Your Hobbies.py
@@ -0,0 +1,18 @@
+hobbies = []
+
+# Add your code below!
+for i in range(3):
+        raw_input("What are your hobbies: ")
+        hobbies.append(hobbies)
+
+print hobbies
+
+#version 2:
+
+hobbies = []
+
+# Add your code below!
+for i in range(3):
+        hobbies.append(str(raw_input("What is a hobby of yours?")))
+        
+print hobbies
