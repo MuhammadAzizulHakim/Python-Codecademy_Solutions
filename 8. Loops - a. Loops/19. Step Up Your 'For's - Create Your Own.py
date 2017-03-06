b175dfade161e59a4aaaf667cc45774f4618beeb
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-06 21:15:25 +0700

    Create 19. Step Up Your 'For's - Create Your Own.py

---
+++ 8. Loops - a. Loops/19. Step Up Your 'For's - Create Your Own.py
@@ -0,0 +1,9 @@
+animals = ['cat', 'alligator', 'mouse', 'shark', 'peacock', 'whale']
+
+print 'You wish to have...'
+for a in animals:
+    if a == 'peacock':
+        print 'A peacock is not a mammal!'
+    print 'A', a
+else:
+    print 'A fine selections of animals!'
