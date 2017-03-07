5476108f0dfc91672ebd76ce77c95e03c60e77df
Muhammad Azizul Hakim <19azizul92@gmail.com>
2017-03-08 00:02:48 +0700

    Create 3. Iteration Nation - The 'in' Operator.py

---
+++ 10. Advanced Topics in Python - a. Advanced Topics in Python/3. Iteration Nation - The 'in' Operator.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "Name": "Aziz",
+    "Pet": "Cat",
+    "Age": 24,
+    "True?": True
+}
+
+for key in my_dict:
+    print key, my_dict[key],
+
+print my_dict.keys()
+print my_dict.values()
